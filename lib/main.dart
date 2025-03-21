1ea57c424fe05a4a245e8757993923d0adc1bb8d
CHARLESJESU <charlesjmng@gmail.com>
2025-03-21 12:38:28 +0530

    I add splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nivetha123/Splashscreen/splashscreen.dart';
 
 import 'login/Login.dart';
 
@@ -39,7 +40,7 @@ class _MyappState extends State<Myapp> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
